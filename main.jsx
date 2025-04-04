8f880ae48ce2ada4df21645d98b7c69ebed4f569
Tanishq <97272471+ultrew@users.noreply.github.com>
2025-04-05 03:28:50 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PreRepeter from './PreRepeter';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PreRepeter />
-  </React.StrictMode>
-);
