162e35a96e6ee0524e91f9f837ec762c875e9f37
Tanishq <97272471+ultrew@users.noreply.github.com>
2025-04-05 02:43:02 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PreRepeter from './PreRepeter';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PreRepeter />
+  </React.StrictMode>
+);
